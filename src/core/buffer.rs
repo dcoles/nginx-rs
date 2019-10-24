2b6e386fc0e7866ad9e30960beba8151fe7bee23
David Coles <coles.david@gmail.com>
2019-10-23 20:52:57 -0700

    buffer: Remove duplication by using default trait methods

--- src/core/buffer.rs
+++ src/core/buffer.rs
@@ -7,17 +7,41 @@ pub trait Buffer {
 
     fn as_ngx_buf_mut(&mut self) -> *mut ngx_buf_t;
 
-    fn as_bytes(&self) -> &[u8];
+    fn as_bytes(&self) -> &[u8] {
+        let buf = self.as_ngx_buf();
+        unsafe { slice::from_raw_parts((*buf).pos, self.len()) }
+    }
 
-    fn len(&self) -> usize;
+    fn len(&self) -> usize {
+        let buf = self.as_ngx_buf();
+        unsafe {
+            let pos = (*buf).pos;
+            let last = (*buf).last;
+            assert!(last > pos);
+            usize::wrapping_sub(last as _, pos as _)
+        }
+    }
 
-    fn set_last_buf(&mut self, last: bool);
+    fn set_last_buf(&mut self, last: bool) {
+        let buf = self.as_ngx_buf_mut();
+        unsafe {
+            (*buf).set_last_buf(if last { 1 } else { 0 });
+        }
+    }
 
-    fn set_last_in_chain(&mut self, last: bool);
+    fn set_last_in_chain(&mut self, last: bool) {
+        let buf = self.as_ngx_buf_mut();
+        unsafe {
+            (*buf).set_last_in_chain(if last { 1 } else { 0 });
+        }
+    }
 }
 
 pub trait MutableBuffer: Buffer {
-    fn as_bytes_mut(&mut self) -> &mut [u8];
+    fn as_bytes_mut(&mut self) -> &mut [u8] {
+        let buf = self.as_ngx_buf_mut();
+        unsafe { slice::from_raw_parts_mut((*buf).pos, self.len()) }
+    }
 }
 
 pub struct TemporaryBuffer(*mut ngx_buf_t);
@@ -36,31 +60,6 @@ impl Buffer for TemporaryBuffer {
 
     fn as_ngx_buf_mut(&mut self) -> *mut ngx_buf_t {
         self.0
-    }
-
-    fn as_bytes(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts((*self.0).pos, self.len()) }
-    }
-
-    fn len(&self) -> usize {
-        unsafe {
-            let pos = (*self.0).pos;
-            let last = (*self.0).last;
-            assert!(last > pos);
-            usize::wrapping_sub(last as _, pos as _)
-        }
-    }
-
-    fn set_last_buf(&mut self, last: bool) {
-        unsafe {
-            (*self.0).set_last_buf(if last { 1 } else { 0 });
-        }
-    }
-
-    fn set_last_in_chain(&mut self, last: bool) {
-        unsafe {
-            (*self.0).set_last_in_chain(if last { 1 } else { 0 });
-        }
     }
 }
 
@@ -86,30 +85,5 @@ impl Buffer for MemoryBuffer {
 
     fn as_ngx_buf_mut(&mut self) -> *mut ngx_buf_t {
         return self.0
-    }
-
-    fn as_bytes(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts((*self.0).pos, self.len()) }
-    }
-
-    fn len(&self) -> usize {
-        unsafe {
-            let pos = (*self.0).pos;
-            let last = (*self.0).last;
-            assert!(last > pos);
-            usize::wrapping_sub(last as _, pos as _)
-        }
-    }
-
-    fn set_last_buf(&mut self, last: bool) {
-        unsafe {
-            (*self.0).set_last_buf(if last { 1 } else { 0 });
-        }
-    }
-
-    fn set_last_in_chain(&mut self, last: bool) {
-        unsafe {
-            (*self.0).set_last_in_chain(if last { 1 } else { 0 });
-        }
     }
 }
